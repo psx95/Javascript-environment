d8b78e6d3a04acb090133a212fde0a27c27de7aa
psx95 <pranav.ps95@hotmail.com>
2018-01-17 01:06:48 +0530

    Added css file to the bundler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// this is the appplication entry point as defined in the webpack's configuration
+import './index.css';
 import numeral from 'numeral';
+
 const num = numeral(1000).format('$0,0.00');
+debugger; //  it is a breakpoint
 console.log(`I would pay ${num} for this awesome course`);
+// since we defined that our webpack should be able to handle all the css files as well, we need to 
+// add line of code that will enable the webapack to handle the css code. This here is done by merely adding the import statemnet for the css file which we wish tpo handle
